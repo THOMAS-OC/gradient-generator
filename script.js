03f00eb3a92b0145a918d604607a611a253abd8c
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-21 19:33:55 +0200

    Copie du code CSS ok et stylisation des inputs ok

--- script.js
+++ script.js
@@ -4,26 +4,35 @@ const colorOne = document.querySelector('input[type="color"]:nth-child(1)')
 const colorTwo = document.querySelector('input[type="color"]:nth-child(2)')
 const body = document.querySelector('body')
 
+const btnCopy = document.querySelector(".btn-copy")
+
 let gradient = "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,212,255,1) 100%)"
 
 console.log(colorTwo.value);
 
 range.addEventListener("change", () => {
-    console.log(range.value)
     angleGradiant.innerText = range.value + "°"
-    gradient = `linear-gradient(${range.value}deg, rgba(2,0,36,1) 0%, rgba(0,212,255,1) 100%)`
-    console.log(body);
-    body.style.background = `linear-gradient(${range.value}deg, rgba(2,0,36,1) 0%, rgba(0,212,255,1) 100%)`
+    gradient = `linear-gradient(${range.value}deg, ${colorOne.value} 0%, ${colorTwo.value} 100%)`
+    body.style.background = gradient
 
 })
 
 colorOne.addEventListener("change", () => {
     console.log(colorOne.value);
-    gradient 
+    gradient = `linear-gradient(${range.value}deg, ${colorOne.value} 0%, ${colorTwo.value} 100%)`
+    body.style.background = gradient
+    colorOne.style.background = colorOne.value
 })
 
 colorTwo.addEventListener("change", () => {
     console.log(colorTwo.value);
+    gradient = `linear-gradient(${range.value}deg, ${colorOne.value} 0%, ${colorTwo.value} 100%)`
+    body.style.background = gradient
+    colorTwo.style.background = colorTwo.value
+})
+
+btnCopy.addEventListener("click", () => {
+    navigator.clipboard.writeText(gradient)
 })
 
 
